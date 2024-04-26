f28ced1e22a678587e22ff1a95b27e3d488e1849
GabrielaGuinez <162721892+GabrielaGuinez@users.noreply.github.com>
2024-04-26 09:09:47 -0300

    entrega quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -1,16 +1,47 @@
-# Replace the "ANSWER HERE" for your answer
-
 def roots(a, b, c):
-    return "ANSWER HERE"
+    d = (b**2-4*a*c)
+    if d > 0:
+        r1 = (-b + (d**0.5)) / 2*a
+        r2 = (-b - (d**0.5)) / 2*a
+        return f"{r1,r2}"
+    elif d == 0:
+        r12 = (-b + (d**0.5)) / 2*a 
+        return f"({r12})"
+    else:
+        return"( )" 
+
+    
 
 
 def value_y(a, b, c, x):
-    return "ANSWER HERE"
+    y = a * x * x + b * x + c
+    return y 
+
 
 
 def to_string(a, b, c):
-    return "ANSWER HERE"
+    if a == 0 and b == 0:
+        return f"f(x) = {c}" 
+    elif a == 0 and c == 0:
+        return f"f(x) = {b} * X"
+    elif b == 0 and c == 0:
+        return f"f(x) = {a} * X^2"
+    elif a == 0:
+        return f"f(x) = {b} * X + {c}"
+    elif b == 0: 
+        return f"f(x) = {a} * X^2 + {c}"
+    elif c == 0:
+        return f"f(x) = {a} * X^2 + {b} * X"
+    else:
+        return f"f(x) = {a} * X^2 + {b} * X + {c}" 
 
 
-def derivation(a, b):
-    return "ANSWER HERE"
+
+def derivation(a, b, c): 
+    A = a * 2
+    if a == 0:
+        return f"f'(x) = {b}"
+    if b == 0:
+        return f"f'(x) = {A} * X"
+    else:
+        return f"f'(x) = {A} * X + {b}"
